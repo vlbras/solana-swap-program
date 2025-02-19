a51327fb5341bf875ff43acf50b96483fa3c3c51
Vlad <vladyslav.braslavskyi@flip.shop>
2025-02-19 22:58:36 +0100

    feat: implement swap

--- programs/swap/src/instructions/mod.rs
+++ programs/swap/src/instructions/mod.rs
@@ -1,3 +1,8 @@
-pub mod initialize;
+pub mod make_offer;
+pub use make_offer::*;
 
-pub use initialize::*;
+pub mod take_offer;
+pub use take_offer::*;
+
+pub mod shared;
+pub use shared::*;
--- programs/swap/src/lib.rs
+++ programs/swap/src/lib.rs
@@ -15,7 +15,18 @@ declare_id!("7PmQEeQWpmLhSANi5zj7cJ6sdGQsE4jaHjRuSsvias9e");
 pub mod swap {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn make_offer(
+        context: Context<MakeOffer>,
+        id: u64,
+        token_a_offered_amount: u64,
+        token_b_wanted_amount: u64,
+    ) -> Result<()> {
+        instructions::make_offer::send_offered_tokens_to_vault(&context, token_a_offered_amount)?;
+        instructions::make_offer::save_offer(context, id, token_b_wanted_amount)
+    }
+
+    pub fn take_offer(context: Context<TakeOffer>) -> Result<()> {
+        instructions::take_offer::send_wanted_tokens_to_maker(&context)?;
+        instructions::take_offer::withdraw_and_close_vault(context)
     }
 }
