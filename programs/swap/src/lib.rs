afc8470ea77b4be4f6e613ab91b7a59c82d6dfc4
Vlad <vladyslav.braslavskyi@flip.shop>
2025-02-19 22:49:19 +0100

    build: initial commit

---
+++ programs/swap/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("7PmQEeQWpmLhSANi5zj7cJ6sdGQsE4jaHjRuSsvias9e");
+
+#[program]
+pub mod swap {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
